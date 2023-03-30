0cc3ba460e077ca28b71efeea0041d95899f9e2c
jaytrive <113422452+jaytrive@users.noreply.github.com>
2023-03-30 08:45:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your code here
+let page=1;
+let heading=document.getElementById('heading');
+let issues=document.getElementById('issues');
+function findIssues() {
+	fetch("https://api.github.com/repositories/1296269/issues?page=$%7BpageNumberHere%7D&per_page=5").then((res) => res.json())
+																											  .then((data) => console.log(data));
+}
+
+function showPrev() {
+	if(page>1){
+		page--;
+		heading.innerText=`Page number ${page}`;
+		findIssues();
+	}
+}
+
+	function showNext() {
+	page++;
+	heading.innerText=`Page number ${page}`;
+	findIssues();
+}
+
