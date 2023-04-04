9daa1eb135f5a862748349106df4ab70e54e8e8d
jaytrive <113422452+jaytrive@users.noreply.github.com>
2023-04-04 23:56:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,10 +3,17 @@ let page=1;
 let heading=document.getElementById('heading');
 let issues=document.getElementById('issues');
 function findIssues() {
-	fetch("https://api.github.com/repositories/1296269/issues?page=$%7BpageNumberHere%7D&per_page=5").then((res) => res.json())
-																											  .then((data) => console.log(data));
+	fetch(`https://api.github.com/repositories/1296269/issues?page=${page}&per_page=5`)
+		.then((res) => res.json())	
+		.then((data) => {
+			issues.innerHTML="";
+			data.forEach(issue => {
+		        const issueItem = document.createElement('li');
+		        issueItem.textContent = issue.title;
+		        issues.appendChild(issueItem);
+		    });
+		});
 }
-
 function showPrev() {
 	if(page>1){
 		page--;
@@ -14,8 +21,7 @@ function showPrev() {
 		findIssues();
 	}
 }
-
-	function showNext() {
+function showNext() {
 	page++;
 	heading.innerText=`Page number ${page}`;
 	findIssues();
